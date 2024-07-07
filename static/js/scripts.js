224bee403b5e9edec11cce73c8d0bce9c16a66f1
rjrato <ricardo.jorge.rato@gmail.com>
2024-07-07 19:30:14 +0100

    CSS update

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -28,3 +28,20 @@ document.querySelectorAll('.nav-link').forEach(item => {
         setTimeout(() => window.location.hash = item.getAttribute('href'), 50);
     });
 });
+
+function showFeedbackMessage() {
+    var message = document.getElementById('feedbackMessage');
+    message.classList.add('show');
+
+    // Remove the class after the animation ends to allow re-triggering
+    setTimeout(function() {
+        message.classList.remove('show');
+    }, 6000); // Match the duration of the animation
+}
+
+// Example: Call this function when the form is submitted
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent actual form submission for demo purposes
+    showFeedbackMessage();
+});
+
